67d49bed5a6bbdafd415ccbc69602714031fbf3a
MxShun <16466322+MxShun@users.noreply.github.com>
2025-03-16 14:08:12 +0900

    fix: path

--- .github/custom/javascript/index.js
+++ .github/custom/javascript/index.js
@@ -1,4 +1,4 @@
 console.log('Index');
 
 const core = require('@actions/core');
-core.setOutput('greeting', 'Hello' + core.getInput('your-name'));
+core.setOutput('greeting', 'Hello' + " " + core.getInput('your-name'));
